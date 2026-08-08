568f889d5e3e0a6d02f903e5f54114ee894ace00
Minsu <asdoo4069@naver.com>
2026-08-08 23:32:32 +0900

    fix: v0.110.1 JoinButton 씬 노드 참조 호환성 수정
    - NButton의 컨트롤러 아이콘 시스템이 TextureRect(%ControllerIcon)에서
      NHotkeyIcon 컴포넌트(%HotkeyIcon)로 개편됨에 따라 GetNode -> FindChild로 변경
      - Duplicate()로 복제된 노드는 씬 고유 이름(%) 캐시에 등록되지 않아
        FindChild(owned: false)로 이름 기반 탐색하도록 수정
    - NJoinFriendRefreshButton이 자체 TextureRect로 버튼 모양을 그리도록
      리디자인됨에 따라, 이 모드가 별도로 생성하던 NinePatchRect 배경 제거
      (두 배경이 겹쳐 보이는 문제 해결)
    
    join flow 및 전투 동기화 검증 완료

--- LanMultiplayer-MS/Components/JoinButton.cs
+++ LanMultiplayer-MS/Components/JoinButton.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.addons.mega_text;
 using MegaCrit.Sts2.Core.ControllerInput;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
 using MegaCrit.Sts2.Core.Nodes.Screens.MainMenu;
 
 namespace LanMultiplayerMS.Components
@@ -21,28 +22,11 @@ namespace LanMultiplayerMS.Components
                 Material = (Material)joinFriendRefreshButton.Material.Duplicate()
             };
 
-            var background = new NinePatchRect
-            {
-                Name = "Background",
-                MouseFilter = MouseFilterEnum.Ignore,
-                Texture = GD.Load<CompressedTexture2D>("res://images/ui/tiny_nine_patch.png"),
-                PatchMarginLeft = 12,
-                PatchMarginTop = 12,
-                PatchMarginRight = 12,
-                PatchMarginBottom = 12,
-                Modulate = joinFriendRefreshButton.SelfModulate,
-                Material = joinButton.Material
-            };
-
-            joinButton.AddChildSafely(background);
-            background.SetAnchorsAndOffsetsPreset(LayoutPreset.FullRect);
-
             foreach (var child in joinFriendRefreshButton.GetChildren())
                 joinButton.AddChildSafely(child.Duplicate());
 
-            var controllerIcon = joinButton.GetNode<TextureRect>("ControllerIcon");
-            controllerIcon.Owner = joinButton;
-            controllerIcon.Position = new Vector2(controllerIcon.Position.X - 12, controllerIcon.Position.Y);
+            var hotkeyIcon = joinButton.FindChild("HotkeyIcon", recursive: true, owned: false) as NHotkeyIcon;
+            hotkeyIcon!.Position = new Vector2(hotkeyIcon.Position.X - 12, hotkeyIcon.Position.Y);
 
             return joinButton;
         }
